15b9f82240352f8f81acc8f22f890dfb91b9e58d
Paulo Hilton <pahbaracho@gmail.com>
2021-11-18 20:07:21 -0300

    Adcionei Estado do Jogo ao SlingShot

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gameStage = ONSling;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,12 +70,14 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameStage !== "Launched"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
-
 
 function mouseReleased(){
     slingshot.fly();
+    gameStage = "Launched";
 }
 
 function keyPressed(){
